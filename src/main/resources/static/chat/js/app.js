bed8128766665c07ee325e520f89aab914692797
jaebeom <jaebum7396@naver.com>
2023-05-14 00:40:46 +0900

    feat. 친구리스트 불러오기 기능 추가

--- src/main/resources/static/chat/js/app.js
+++ src/main/resources/static/chat/js/app.js
@@ -1,134 +1,196 @@
-const backendUrl = "http://192.168.0.8:9090";
-const clientDomainCd = 1;
-const clientUrl = "192.168.0.8:7089";
-const mainUrl = window.location.href.split('http://')[1];
-
-var sock = new SockJS(backendUrl+"/ws-stomp?userCd="+$('#USER_CD').val());
-var wsClient = Stomp.over(sock);
-var reconnect = 0;
-
-if (mainUrl.indexOf('.com') > 0 || mainUrl.indexOf('.co.kr')) {
-	requestDomain = mainUrl.split('/')[0] + '/' + mainUrl.split('/')[1];
-} else {
-	requestDomain = mainUrl.split('/')[0];
-}
-
 $(document).ready(function() {
-	var preloadbg = document.createElement("img");
-	preloadbg.src = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/245657/timeline1.png";
+    var preloadbg = document.createElement("img");
+    preloadbg.src = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/245657/timeline1.png";
 
-	$("#searchfield").focus(function() {
-		if ($(this).val() == "Search contacts...") {
-			$(this).val("");
-		}
-	});
-	$("#searchfield").focusout(function() {
-		if ($(this).val() == "") {
-			$(this).val("Search contacts...");
+    $("#searchfield").focus(function() {
+        if ($(this).val() == "Search contacts...") {
+            $(this).val("");
+        }
+    });
+    $("#searchfield").focusout(function() {
+        if ($(this).val() == "") {
+            $(this).val("Search contacts...");
 
-		}
-	});
-	$("#sendmessage input").focus(function() {
-		if ($(this).val() == "Send message...") {
-			$(this).val("");
-		}
-	});
-	$("#sendmessage input").focusout(function() {
-		if ($(this).val() == "") {
-			$(this).val("Send message...");
+        }
+    });
+    $("#sendmessage input").focus(function() {
+        if ($(this).val() == "Send message...") {
+            $(this).val("");
+        }
+    });
+    $("#sendmessage input").focusout(function() {
+        if ($(this).val() == "") {
+            $(this).val("Send message...");
 
-		}
-	});
-	$('#sendmessage #msg').keydown((e) => {
-		if (e.keyCode == 13) {//키가 13이면 실행 (엔터는 13)
-			sendMessageHub(document.getElementById("msg").value, isValidURL(document.getElementById("msg").value)?'5' : '1')
-		}
-	});
-	wsClientConnectHub();
+        }
+    });
+    $('#sendmessage #msg').keydown((e) => {
+        if (e.keyCode == 13) {//키가 13이면 실행 (엔터는 13)
+            sendMessageHub(document.getElementById("msg").value, isValidURL(document.getElementById("msg").value)?'5' : '1')
+        }
+    });
+    getFriendsWithPageable(0);
 });
 
-function wsClientConnectHub() {
-	console.log('wsClientConnectHub start')
-	wsClientConnect()
-	console.log('wsClientConnectHub done')
-}
-function wsClientConnect() {
-	wsClient.connect({}, function (frame) {
-		let mainChannelCd = 0;
-		let wssKey = $('#WSS_KEY').val();
-		wsClient.subscribe("/sub/channel/"+clientDomainCd+"/"+0, function (message) {
-			console.log('connect.subscribe', message);
-			let recv = JSON.parse(message.body);
-			recvMessage(recv);
-		});
-		wsClient.send(
-			"/pub/message"
-			, {}
-			, JSON.stringify({
-				transferType: 1
-				,domainCd: clientDomainCd
-				,channelCd: mainChannelCd
-				,sender: wssKey
-		}));
-	}, function (error) {
-		if (reconnect++ <= 5) {
-			setTimeout(function () {
-				console.log("connection reconnect");
-				sock = new SockJS(backendUrl+"/ws-stomp");
-				wsClient = Stomp.over(sock);
-				wsClientConnect();
-			}, 10 * 1000);
-		}
-	});
-}
-function sendMessage(channelCd, message) {
-	let mainChannelCd = 0;
-	let wssKey = $('#WSS_KEY').val();
-	wsClient.send(
-		"/pub/message"
-		, {}
-		, JSON.stringify({
-			transferType: 4
-			, domainCd: clientDomainCd
-			, channelCd: channelCd
-			, sender: wssKey
-			, message: message
-		})
-	);
-	document.getElementById("msg").value = ''; // 입력창 초기화
-}
-function recvMessage(msg){
 
-	console.log(msg)
-	alarmMaker(msg);
+$(document).ready(function() {
+    $(".left_side_btn").click(function() {
+        leftSideMenuOpen();
+        loadChannelUserListHub();
+        window.location.hash = "#open";
+    })
+})
+
+window.onhashchange = function() {
+    if (location.hash != "#open") {
+        leftSideMenuClose()
+    }
+};
+
+function leftSideMenuOpen(){
+    $("#menu,.page_cover,html").addClass("open");
 }
-function alarmMaker(p_message){
-	console.log('alarmMaker>>>>>>>>>>>>', p_message)
-	let html = '';
-	html+="<div class='alarm "+p_message.messageCd+"' style='max-width: 400px; display:flex; border-radius: 5px; margin: 5px; background-color:white;' onclick='closeAlarm(this)'>"
-	//html+=		"<div style='width: 50px; height:50px; background:url(\""+p_message.userInfo.userProfile.profileImgUrl+"\");background-size: cover; border-radius:5px;'></div>"
-	html+=		"<div class='oneLineAlarm' style='margin: 10px; font-size: 15px; font-weight: 600;'>"+p_message.message+"</div>"
-	html+="</div>"
 
-	console.log(html)
-
-	$('#alarm_popup #chat_contents').prepend(html);
-
-	setTimeout(function() {
-		$(".alarm."+p_message.messageCd).fadeOut('slow', function() {
-			$(this).remove();
-		});
-	}, 50000);
+function leftSideMenuClose(){
+    $("#menu,.page_cover,html").removeClass("open");
 }
-function findAllChannel() {
 
+$(document).ready(function(){
+    $("#menu ul.channel_user_list").hide();
+    $("#menu ul.nav li a .channel_user_btn").click(function(){
+        $("ul", '.channel_user_list').slideToggle("fast");
+    })
+})
+
+//채팅방 보이기 함수
+function chatRoomVisible(p_flag) {
+    $("#chat_header p").addClass("animate"); $("#chat_header").addClass("animate");;
+    $("#chat-messages").addClass("animate");
+    $('.cx, .cy').addClass('s1');
+    $('.cx, .cy').addClass('s2');
+    $('.cx, .cy').addClass('s3');
+
+    $('#tab_container').fadeOut();
+    $('#chatview').fadeIn();
+
+    $('#close_chat').unbind("click").click(function() {
+        $("#chat-messages, #chat_header, #chat_header p").removeClass("animate");
+        $('.cx, .cy').removeClass("s1 s2 s3");
+        $('#chatview').fadeOut();
+        $('#tab_container').fadeIn();
+        $('#OPEN_CHANNEL_CD').val('');
+        if("channel" == p_flag){
+            getChannelsWithPageable('0')
+        }else{
+            getFriendsWithPageable('0');
+        }
+    });
+    closePopupProfile();
 }
-function createChannel() {
 
+//친구리스트 페이징
+var OPEN_FRIEND_LIST_YN = false;
+function getFriendsWithPageable(p_page) {
+    console.log('getFriendsWithPageable>>>>>>>>>>>>')
+    $('#channel_list_container').css('display', 'none')
+    $('#friend_list_container').css('display', 'block')
+    if (OPEN_FRIEND_LIST_YN == false) {
+        OPEN_FRIEND_LIST_YN = true;
+        if (!p_page) {
+            var p_page = $("#tab_container input[name='current_page_num']").val();
+            if(p_page==0||p_page==''){
+                getMyInfo();
+            }
+        } else {
+            if (p_page == '0') {
+                $("#friend_list_container").html('');
+                getMyInfo();
+            }
+        }
+        axios.get(backendUrl+'/chat/friends', {
+            page: p_page,
+            size: 11
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': localStorage.getItem("token"),
+            }
+        })
+        .then(response => {
+            console.log('getFriendsWithPageableResp', response)
+
+            let friendList = response.result.friendList
+            $("#tab_container input[name='current_page_num']").val(response.result.p_page);
+            friendMakerHub(friendList, $("#friend_list_container"), true);
+
+            OPEN_FRIEND_LIST_YN = false;
+        })
+        .catch(error => {
+            alert('로그인 실패');
+            console.error(error);
+        });
+    }
+
+    $('#friend_list_container').scroll(function() {
+        let noMore = false;
+        let scrollTop = $('#friend_list_container').scrollTop();
+        let innerHeight = $('#friend_list_container').height();
+        let scrollHeight = $('#friend_list_container')[0].scrollHeight;
+        let current_page_num = $("#tab_container input[name='current_page_num']").val()
+        if (!noMore) {
+            if (scrollTop + innerHeight >= scrollHeight) {
+                if (innerHeight != 0) {
+                    //스크롤이 바닥치면 뭐할지 여기에 정의 시작
+                    $('#friend_list_container').scrollTop(scrollHeight - 150);
+                    $("#tab_container input[name='current_page_num']").val(Number(current_page_num) + Number(1))
+                    getFriendsWithPageable();
+                    noMore = (true);
+                }
+            }
+        }
+    });
 }
-function enterChannel(channelCd) {
 
+async function friendMakerHub(friendList, p_obj, rowClickActivate){
+    console.log('friendMakerHub start')
+    for (var i = 0; i < friendList.length; i++) {
+        let friend = friendList[i];
+        await p_obj.append(friendMaker(friend, rowClickActivate));
+    }
+    console.log('friendMakerHub done')
 }
-function findChannel(){
 
+function friendMaker(friend, rowClickActivate) {
+    console.log(friend)
+    let htmlText ="";
+    htmlText += "<div class='chat_row friend " + friend.userCd + "'>";
+    htmlText += 	"<input class='USER_CD' id='USER_CD' name='USER_CD' type='hidden' value='" + friend.userCd + "'/>";
+    htmlText +=		"<div class='profile_container'>"
+    htmlText += 		profileMaker(friend,' left:auto; top:auto;');
+    htmlText += 	"</div>";
+    htmlText += 	"<div onclick='rowClick(this, "+rowClickActivate+");' style='padding:10px;display:flex;flex-direction:column;justify-content:space-between;font-size:15px;'>";
+    htmlText += 		"<strong class='friend_alias alias' style='color: #597a96;'>" + (friend.userNickNm!=null? friend.userNickNm : friend.userNm) + "</strong>";
+    htmlText += 		"<strong class='friend_message'>" + (friend.userMessage!=null? friend.userMessage:"") + "</strong>";
+    htmlText += 	"</div>";
+    htmlText += 	"<div style='margin: auto;'>";
+    htmlText += 		"<input id='check_"+friend.userCd+"' class='friend_check' type='checkbox' style='' value='" + friend.userCd + "'/>";
+    htmlText +=     	"<label for='check_"+friend.userCd+"' class='friend_check_label'></label>"
+    htmlText += 	"</div>";
+    htmlText += "</div>";
+    return htmlText;
+}
+
+//채팅리스트 및 친구 리스트 프로필 생성
+function profileMaker(friend, imgSizestr){
+    let htmlText='';
+    if(friend.userProfile.profileImgUrl){
+        htmlText += 	"<div class='profile_img' style='"+imgSizestr+" '>"
+        htmlText += 		"<img src='"+friend.userProfile.profileImgUrl+"'>";
+        htmlText += 	"</div>";
+    }else{
+        htmlText += 	"<div class='profile_img' style='"+imgSizestr+"'>";
+        htmlText += 		"<img src='chat/image/face_common.jpg'>";
+        htmlText += 	"</div>";
+    }
+    return htmlText;
 }
