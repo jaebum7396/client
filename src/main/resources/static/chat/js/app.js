6cce00a9b41bccbb64b0e5b75cb11811b2b8835e
jaebeom <jaebum7396@naver.com>
2023-07-20 21:54:55 +0900

    fix. 로그 활성화
    Signed-off-by: jaebeom <jaebum7396@naver.com>

--- src/main/resources/static/chat/js/app.js
+++ src/main/resources/static/chat/js/app.js
@@ -1,5 +1,5 @@
-const debugYn = CURRENT_PROFILE == 'local' ? true : false;
-//const debugYn = true;
+//const debugYn = CURRENT_PROFILE == 'local' ? true : false;
+const debugYn = true;
 
 function getCurrentTime(){
     let today = new Date();
