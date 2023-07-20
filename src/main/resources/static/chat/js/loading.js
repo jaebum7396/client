fc915937ef8012be2a40d22c3b31bd6a16f9b4e2
jaebum <jaebum7396@naver.com>
2023-07-20 17:25:24 +0900

    style. 당겨 새로고침 ui 추가

--- src/main/resources/static/chat/js/loading.js
+++ src/main/resources/static/chat/js/loading.js
@@ -28,25 +28,29 @@ function swipeStart(e) {
 }
 
 function swipeEnd(e) {
-    if (document.body.scrollTop === 0 && !isLoading) {
+    // 내부 요소가 스크롤되고 있을 때는 로딩을 실행하지 않음
+    if (e.target === main && main.scrollTop === 0 && !isLoading) {
         for (const card of cards) card.style.transform = `rotateX(0deg)`;
     }
 }
 
 function swipe(e) {
-    if (typeof e["changedTouches"] !== "undefined") {
-        let touch = e.changedTouches[0];
-        pCurrent.x = touch.screenX;
-        pCurrent.y = touch.screenY;
-    } else {
-        pCurrent.x = e.screenX;
-        pCurrent.y = e.screenY;
-    }
-    let changeY = pStart.y < pCurrent.y ? Math.abs(pStart.y - pCurrent.y) : 0;
-    const rotation = changeY < 100 ? changeY * 30 / 100 : 30;
-    if (main.scrollTop === 0) {
-        if (changeY > 100) loading();
-        for (const card of cards) card.style.transform = `rotateX(${rotation}deg)`;
+    // 내부 요소가 스크롤되고 있을 때는 로딩을 실행하지 않음
+    if (e.target === main) {
+        if (typeof e["changedTouches"] !== "undefined") {
+            let touch = e.changedTouches[0];
+            pCurrent.x = touch.screenX;
+            pCurrent.y = touch.screenY;
+        } else {
+            pCurrent.x = e.screenX;
+            pCurrent.y = e.screenY;
+        }
+        let changeY = pStart.y < pCurrent.y ? Math.abs(pStart.y - pCurrent.y) : 0;
+        const rotation = changeY < 100 ? changeY * 30 / 100 : 30;
+        if (main.scrollTop === 0) {
+            if (changeY > 100) loading();
+            for (const card of cards) card.style.transform = `rotateX(${rotation}deg)`;
+        }
     }
 }
 
